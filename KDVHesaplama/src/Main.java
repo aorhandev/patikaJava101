544144b3a9707200f7616f79e7bc7eb4f3c70451
Ahmet Orhan <muh.aorhan@gmail.com>
2022-05-23 09:45:22 +0300

    kdv

--- KDVHesaplama/src/Main.java
+++ KDVHesaplama/src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        int tutar;
+        double tutar;
         double kdv;
 
         Scanner scanner = new Scanner(System.in);
@@ -13,14 +13,16 @@ public class Main {
         tutar = scanner.nextInt();
 
         if (tutar <= 1000) {
-            kdv = 1.8;
+            kdv = 1.18;
         } else {
-            kdv = 0.8;
+            kdv = 1.08;
         }
 
+        double sonuc = tutar * kdv ;
+
         System.out.println("KDV'siz Fiyat= " + tutar);
-        System.out.println("KDV'li Fiyat= " + (tutar * kdv));
-        System.out.println("KDV tutarı= " + kdv);
+        System.out.println("KDV'li Fiyat= " + sonuc);
+        System.out.println("KDV tutarı= " + (sonuc - tutar));
 
     }
 
